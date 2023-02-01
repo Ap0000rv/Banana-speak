3bf2dcf34b0519ba81058364bbfe6bb943ce2715
Ap0000rv <123739980+Ap0000rv@users.noreply.github.com>
2023-02-01 18:41:43 +0530

    TalkBanana

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output-box");
 
-// var serverURL = "https://api.funtranslations.com/translate/minion.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
 function getTransitionalURL(input) {
-	return serverURL + "?text=" + input
+	return serverURL + "?text=" + input;
 }
 
 function errorHandler(error) {
@@ -18,8 +18,8 @@ function clickHandler() {
 	var inputText = txtInput.value;
 
 	fetch(getTransitionalURL(inputText))
-		.then(response => response.json())
-		.then(json => {
+		.then((response) => response.json())
+		.then((json) => {
 			outputDiv.innerText = json.contents.translated;
 		})
 		.catch(errorHandler);

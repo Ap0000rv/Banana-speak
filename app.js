cf22dabec74a75936f2d67ea628e240d1a89ae89
Ap0000rv <123739980+Ap0000rv@users.noreply.github.com>
2023-02-01 15:17:42 +0530

    Add JS

--- app.js
+++ app.js
@@ -2,11 +2,27 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output-box");
 
+// var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function getTransitionalURL(input) {
+	return serverURL + "?text=" + input
+}
+
+function errorHandler(error) {
+	console.log("Error Occurs ", error);
+}
+
 function clickHandler() {
-	if (txtInput.value === "") return;
-	else {
-		outputDiv.innerText = "Banana Translation is: " + txtInput.value;
-	}
+	var inputText = txtInput.value;
+
+	fetch(getTransitionalURL(inputText))
+		.then(response => response.json())
+		.then(json => {
+			outputDiv.innerText = json.contents.translated;
+		})
+		.catch(errorHandler);
 }
 
 btnTranslate.addEventListener("click", clickHandler);
